b68e08b4f2ec8b011ceef4aaff4c25f978547ea0
Heal <ictgilrovero01000111@gmail.com>
2022-04-12 09:20:25 +0800

    Variable Naming

--- modules/enter_name_of_the_file.cpp
+++ modules/enter_name_of_the_file.cpp
@@ -6,7 +6,7 @@ import <string>;
 
 export auto enter_file_name() -> std::string {
 
-  std::string enter_fname = "";
+  std::string enter_proper_file_name = "";
 
   std::string dot_html_extn = ".html";
 
@@ -18,29 +18,29 @@ export auto enter_file_name() -> std::string {
   bool is_valid_key = false;
 
   std::cout << "Enter Proper File Name (Without Spaces): ";
-  std::cin >> enter_fname;
+  std::cin >> enter_proper_file_name;
 
   std::cout << "Confirm File Name     :\n";
-  std::cout << "\"" << enter_fname << "\"\n";
+  std::cout << "\"" << enter_proper_file_name << "\"\n";
   std::cout << "Enter [Y/y] to Confirm: ";
   std::cin >> enter_key;
 
   if((enter_key == confirmation_key_big_y) || (enter_key == confirmation_key_small_y)) {
     is_valid_key = true;
-    std::cout << "\n\"" << enter_fname << "\" successfully created.";
+    std::cout << "\n\"" << enter_proper_file_name << "\" successfully created.";
   }
 
   if(is_valid_key == false) {
-    std::cout << "\nInvalid Format -> \"" << enter_fname << "\" file failed to create.";
+    std::cout << "\nInvalid Format -> \"" << enter_proper_file_name << "\" file failed to create.";
     return enter_file_name();
   }
 
   for(size_t i = 0; i < dot_html_extn[i] != '\0'; ++i) {
-    enter_fname.push_back(dot_html_extn[i]);
+    enter_proper_file_name.push_back(dot_html_extn[i]);
   }
 
-  std::cout << "New File Name: " << enter_fname;
+  std::cout << "New File Name: " << enter_proper_file_name;
 
-  return enter_fname;
+  return enter_proper_file_name;
 
 }
